e69f565dfad9bca54da52f961894672ccee7cc39
chezbro <ericchesbrough@gmail.com>
2024-09-10 12:58:45 -0700

    deploy1

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/tripshaman/:path*',
-          destination: 'https://tripshaman-n5an.vercel.app//:path*',
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: '/tripshaman/:path*',
+        destination: 'https://tripshaman-n5an.vercel.app/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
