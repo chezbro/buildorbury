401699021bc246259964549023f884fbd9164e76
chezbro <ericchesbrough@gmail.com>
2024-09-11 12:46:04 -0700

    trying to add an additional rewrite path for sub pages

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         destination: 'https://tripshaman-n5an.vercel.app/:path*',
       },
       {
+        source: '/tripshaman/:path*',
+        destination: 'https://tripshaman-n5an.vercel.app/tripshaman/:path*',
+      },      
+      {
         source: '/traderschool/:path*',
         destination: 'https://traderschool.vercel.app/:path*',
       },
